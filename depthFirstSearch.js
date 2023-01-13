001602d81c3277d5f9f91704a890bec1c582e168
masudKhanGit <masud.git.developer@gmail.com>
2023-01-13 21:15:30 +0600

    change depthfirst

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -1,18 +1,38 @@
+// const depthFirstPrint = (graph, soruce) => {
+//     const stack = [ soruce ];
+
+//     while(stack.length > 0) {
+//         const current = stack.pop();
+//         console.log(current);
+
+//         for(let neighbor of graph[current]) {
+//             stack.push(neighbor);
+//         }
+//     }
+// }
+
+// const graph = {
+//     a : ['c', 'b'],
+//     b : ['d'],
+//     c : ['e'],
+//     d : ['f'],
+//     e : [],
+//     f : [],
+// };
+
+// depthFirstPrint(graph, 'a');
+
+// ---------------------------------------------------
+
 const depthFirstPrint = (graph, soruce) => {
-    const stack = [ soruce ];
-
-    while(stack.length > 0) {
-        const current = stack.pop();
-        console.log(current);
-
-        for(let neighbor of graph[current]) {
-            stack.push(neighbor);
-        }
+    console.log(soruce);
+    for(let neighbor of graph[soruce]) {
+        depthFirstPrint(graph, neighbor);
     }
 }
 
 const graph = {
-    a : ['c', 'b'],
+    a : ['b', 'c'],
     b : ['d'],
     c : ['e'],
     d : ['f'],
