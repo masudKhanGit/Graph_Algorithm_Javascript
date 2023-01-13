1e17006340e1acb97e6a37592edb8a7ffb87b718
masudKhanGit <masud.git.developer@gmail.com>
2023-01-13 20:59:33 +0600

    DepthFirstPrint

---
+++ depthFirstSearch.js
@@ -0,0 +1,23 @@
+const depthFirstPrint = (graph, soruce) => {
+    const stack = [ soruce ];
+
+    while(stack.length > 0) {
+        const current = stack.pop();
+        console.log(current);
+
+        for(let neighbor of graph[current]) {
+            stack.push(neighbor);
+        }
+    }
+}
+
+const graph = {
+    a : ['c', 'b'],
+    b : ['d'],
+    c : ['e'],
+    d : ['f'],
+    e : [],
+    f : [],
+};
+
+depthFirstPrint(graph, 'a');
